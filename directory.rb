86c6a56f5b82882cd35b1587032376e9a875f6be
EC2 Default User <ec2-user@ip-172-31-19-5.us-east-2.compute.internal>
2018-09-26 13:57:13 +0000

    Add a list of new students best

--- directory.rb
+++ directory.rb
@@ -1,30 +1,32 @@
-student_count = 11
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Micheal Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
-def print_header
-  puts "The students of Villians Academy"
-  puts "-------------"
-end
-def print(names)
-names.each do |name|
-    puts name
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
   end
-end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  students
 end
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
