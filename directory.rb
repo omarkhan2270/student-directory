038a8a613fc89e3d079c1c01de831f03d3610d48
EC2 Default User <ec2-user@ip-172-31-19-5.us-east-2.compute.internal>
2018-09-26 12:46:30 +0000

    Add a list of new students

--- directory.rb
+++ directory.rb
@@ -1,16 +1,23 @@
+student_count = 11
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Micheal Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 puts "The students of Villians Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Micheal Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have "
-print 11 
-puts " great students"
+students.each do |student|
+    puts student
+end
+puts students[9]
+puts students[10]
+puts students[11]
+puts "Overall, we have #{student_count} great students"
